c2f329c862d3ef498cd0580ea3d1a0a02b216fd1
yusuke-calm0418 <uyhnt73w@gmail.com>
2025-10-31 13:27:45 +0900

    fix: MySQLのポート設定を3307に変更し、Tailwind CSSの設定から不要なパスを削除

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -8,7 +8,6 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
-        './resources/js/Pages/Auth/Login.jsx',
     ],
 
     theme: {
