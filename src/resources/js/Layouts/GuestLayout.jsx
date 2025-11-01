efd135630b40bcea90bdd83cc708886217188105
yusuke-calm0418 <uyhnt73w@gmail.com>
2025-11-01 10:38:38 +0900

    refactor: 不要なコードを削除し、GuestLayoutコンポーネントの可読性を向上

--- src/resources/js/Layouts/GuestLayout.jsx
+++ src/resources/js/Layouts/GuestLayout.jsx
@@ -4,12 +4,6 @@ import { Link } from '@inertiajs/react';
 export default function GuestLayout({ children }) {
     return (
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            {/* <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
-            </div> */}
-
             <div className="w-full overflow-hidden bg-white sm:rounded-lg">
                 {children}
             </div>
