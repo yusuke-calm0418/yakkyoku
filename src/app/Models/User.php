c7b8f35b544f451c821cd85a798588415ce02550
yusuke-calm0418 <uyhnt73w@gmail.com>
2025-11-01 20:16:42 +0900

    fix: 不要なコメントを削除し、コードの可読性を向上

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable; // ←これが重要！
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
